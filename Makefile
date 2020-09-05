7252326d1ea1dc416753cf509aedc91c5d151927
Nikita Koydo <cheeeasy2501@gmail.com>
2020-09-06 00:18:01 +0300

    Configured Laravel and Vue with Docker on Ubuntu

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 CURRENT_ID=$([[ $(id -u) -gt 9999 ]] && echo "root" || id -u)
 CURRENT_GROUP=$([[ $(id -g) -gt 9999 ]] && echo "root" || id -g)
 
+SUDO := CURRENT_USER=${CURRENT_ID}:${CURRENT_GROUP}
 DC := CURRENT_USER=${CURRENT_ID}:${CURRENT_GROUP} docker-compose
 FPM := $(DC) exec php-fpm
 ARTISAN := $(FPM) php artisan
@@ -29,4 +30,9 @@ composer-install:
 dump-autoload:
 	@$(FPM) composer dump-autoload
 
-deploy: env build start composer-install dump-autoload keygen migrate
+frontend:
+	@$(SUDO) npm run watch-poll
+
+restart: stop build start frontend
+
+deploy: env build start composer-install dump-autoload keygen migrate frontend
---
+++ public/dist/js/af3b86e234f0ec1ccbcd.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[15],{
+
+/***/ "./resources/js/pages/welcome.vue~":
+/*!*****************************************!*\
+  !*** ./resources/js/pages/welcome.vue~ ***!
+  \*****************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module parse failed: Unexpected token (1:0)\\nYou may need an appropriate loader to handle this file type.\\n> <template>\\n|   <div>\\n|     <div class=\\\"top-right links\\\">\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL3Jlc291cmNlcy9qcy9wYWdlcy93ZWxjb21lLnZ1ZX4uanMiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./resources/js/pages/welcome.vue~\n");
+
+/***/ })
+
+}]);
