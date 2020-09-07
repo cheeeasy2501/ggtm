491ecbc2d467dac810c03822e13c8c43da49e893
mr-Iva <iva.daoda@gmail.com>
2020-09-07 23:44:00 +0300

    fix webpack bug

--- webpack.mix.js
+++ webpack.mix.js
@@ -33,32 +33,6 @@ mix.webpackConfig({
     chunkFilename: 'dist/js/[chunkhash].js',
     path: mix.config.hmr ? '/' : path.resolve(__dirname, './public/build')
   },
-  rules: [
-    {
-      test: /\.s(c|a)ss$/,
-      use: [
-        'vue-style-loader',
-        'css-loader',
-        {
-          loader: 'sass-loader',
-          // Requires sass-loader@^7.0.0
-          options: {
-            implementation: require('sass'),
-            fiber: require('fibers'),
-            indentedSyntax: true // optional
-          },
-          // Requires sass-loader@^8.0.0
-          options: {
-            implementation: require('sass'),
-            sassOptions: {
-              fiber: require('fibers'),
-              indentedSyntax: true // optional
-            },
-          },
-        },
-      ],
-    },
-  ],
 })
 
 mix.then(() => {
@@ -67,7 +41,7 @@ mix.then(() => {
   }
 })
 
-function publishAseets () {
+function publishAseets() {
   const publicDir = path.resolve(__dirname, './public')
 
   if (mix.inProduction()) {
